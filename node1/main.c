9fc6112752626fcc216540048a640546ac88e07f
HerAmu <[hermanba@stud.ntnu.no]>
2018-08-30 16:19:27 +0200

    new proj

---
+++ node1/main.c
@@ -0,0 +1,22 @@
+#ifndef F_CPU
+#define F_CPU 1000000
+
+
+#include <util/delay.h>
+#include "led.h"
+
+int main(){
+	led_init();
+
+	while(1){
+		led_turn_on();
+		_delay_ms(1000);
+
+		led_turn_off();
+		_delay_ms(1000);
+	}
+
+	return 0;
+}
+
+#endif
