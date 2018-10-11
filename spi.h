aa6059040d3b2c572e51ba8b2949d3b850b915f0
larsmmo <larsmmo@stud.ntnu.no>
2018-10-11 12:24:08 +0200

    small fixes

--- spi.h
+++
@@ -1,8 +0,0 @@
-#ifndef SPI_H
-#define SPI_H
-
-void SPI_init(void);		//SPI initialization (as a Master)
-void SPI_write(char cData);	//Send a message to slave through SPI comm
-char SPI_read(void);		//Read a message from slave through SPI comm
-
-#endif // SPI_H
